60ce50a208337166e6ad96e2f53eb30c76289e34
zenon <zzgocc@gmail.com>
2017-08-09 22:21:30 +0800

    - update

--- HZMethodSwizzling/HZMethodSwizzling/HZUIChecker.m
+++ HZMethodSwizzling/HZMethodSwizzling/HZUIChecker.m
@@ -13,7 +13,7 @@
 #import <dlfcn.h>
 #import <mach-o/ldsyms.h>
 
-static void myForwardInvocation(id slf, SEL selector, NSInvocation *invocation);
+static void HZUICheckerForwardInvocation(__unsafe_unretained id slf, SEL selector, NSInvocation *invocation);
 
 @implementation HZUIChecker
 
@@ -123,25 +123,16 @@ static void myForwardInvocation(id slf, SEL selector, NSInvocation *invocation);
     
     //_objc_msgForward用于消息转发
     //当将原来的IMP替换成_objc_msgForward时，直接进行消息的转发，调用forwardInvocation:
-    IMP msgForwardIMP = _objc_msgForward;
-    
-    if (typeDescription[0] == '{')//类似 {NSObject=#} 的字符串是一个对象的结构
-    {
-        NSMethodSignature *methodSignature = [NSMethodSignature signatureWithObjCTypes:typeDescription];
-        if ([methodSignature.debugDescription rangeOfString:@"is special struct return? YES"].location != NSNotFound) {//通过检测对应字符串判断有问题的struct
-            //对于某些架构某些 struct，返回值必须使用 _objc_msgForward_stret 代替 _objc_msgForward
-            msgForwardIMP = (IMP)_objc_msgForward_stret;
-        }
-    }
+    IMP msgForwardIMP = getMsgForwardIMP(cls, selector);
     
     //替换 selector. 将使用 msgForwardIMP 的 IMP
     class_replaceMethod(cls, selector, msgForwardIMP, typeDescription);
     
     //判断，如果 forwardInvocation: 方法的 IMP 和 myForwardInvocation 的 IMP 不一样，则 replace
-    if (class_getMethodImplementation(cls, @selector(forwardInvocation:)) != (IMP)myForwardInvocation)
+    if (class_getMethodImplementation(cls, @selector(forwardInvocation:)) != (IMP)HZUICheckerForwardInvocation)
     {   //将forwardInvocation:替换成我们自定义的方法myForwardInvocation
         //这样在消息重定位时便会运行到myForwardInvocation，在这个方法里做线程的判断处理。
-        class_replaceMethod(cls, @selector(forwardInvocation:), (IMP)myForwardInvocation, typeDescription);
+        class_replaceMethod(cls, @selector(forwardInvocation:), (IMP)HZUICheckerForwardInvocation, typeDescription);
     }
     
     //是否能响应方法
@@ -158,13 +149,48 @@ static void myForwardInvocation(id slf, SEL selector, NSInvocation *invocation);
     }
 }
 
-static void myForwardInvocation(id slf, SEL selector, NSInvocation *invocation)
+
+static IMP getMsgForwardIMP(Class class,SEL selector){
+    IMP msgForwardIMP = _objc_msgForward;
+#if !defined(__arm64__)
+    // As an ugly internal runtime implementation detail in the 32bit runtime, we need to determine of the method we hook returns a struct or anything larger than id.
+    // https://developer.apple.com/library/mac/documentation/DeveloperTools/Conceptual/LowLevelABI/000-Introduction/introduction.html
+    // https://github.com/ReactiveCocoa/ReactiveCocoa/issues/783
+    // http://infocenter.arm.com/help/topic/com.arm.doc.ihi0042e/IHI0042E_aapcs.pdf (Section 5.4)
+    Method method = class_getInstanceMethod(class, selector);
+    const char *encoding = method_getTypeEncoding(method);
+    
+    
+    //    NSMethodSignature *methodSignature = [NSMethodSignature signatureWithObjCTypes:typeDescription];
+    //    if ([methodSignature.debugDescription rangeOfString:@"is special struct return? YES"].location != NSNotFound) {//通过检测对应字符串判断有问题的struct
+    //        msgForwardIMP = (IMP)_objc_msgForward_stret;
+    //    }
+    
+    BOOL methodReturnsStructValue = encoding[0] == _C_STRUCT_B;
+    if (methodReturnsStructValue) {
+        @try {
+            NSUInteger valueSize = 0;
+            NSGetSizeAndAlignment(encoding, &valueSize, NULL);//取得对象的大小
+            
+            if (valueSize == 1 || valueSize == 2 || valueSize == 4 || valueSize == 8) {
+                methodReturnsStructValue = NO;
+            }
+        } @catch (__unused NSException *e) {}
+    }
+    if (methodReturnsStructValue) { //对于某些架构某些 struct，返回值必须使用 _objc_msgForward_stret 代替 _objc_msgForward
+        msgForwardIMP = (IMP)_objc_msgForward_stret;
+    }
+    
+
+#endif
+    return msgForwardIMP;
+}
+static void HZUICheckerForwardInvocation(__unsafe_unretained id slf, SEL selector, NSInvocation *invocation)
 {
     if (![NSThread currentThread].isMainThread)
     {
         NSLog(@"%@ ",[NSThread callStackSymbols]);
-        NSLog(@"操作不在主线程");
-        assert([NSThread currentThread].isMainThread);
+        NSCAssert([NSThread currentThread].isMainThread, @"操作不在主线程");
     }
     
     //正常执行的时候回通过ORIG_前缀名获取到当前函数的原始方法
